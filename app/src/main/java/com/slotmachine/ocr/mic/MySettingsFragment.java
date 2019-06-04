e9e8709bd065dee14edcbfc90383c58d09344f8f
Alexander Powell <alexanderpowell@Alexanders-MacBook-Pro-2.local>
2019-06-04 14:39:01 +0700

    preferences page updates

--- app/src/main/java/com/slotmachine/ocr/mic/MySettingsFragment.java
+++ app/src/main/java/com/slotmachine/ocr/mic/MySettingsFragment.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.Preference.SummaryProvider;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MySettingsFragment extends PreferenceFragmentCompat {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore database;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -19,6 +27,8 @@ public class MySettingsFragment extends PreferenceFragmentCompat {
 
         EditTextPreference editTextPreference = findPreference("email_recipient");
         EditTextPreference minimum_value = findPreference("minimum_value");
+        Preference sign_out_preference = findPreference("sign_out_button");
+        Preference change_password_button = findPreference("change_password_button");
 
         if (editTextPreference != null) {
             editTextPreference.setSummaryProvider(new SummaryProvider<EditTextPreference>() {
@@ -43,6 +53,35 @@ public class MySettingsFragment extends PreferenceFragmentCompat {
                     }
             );
         }
+
+        if (sign_out_preference != null) {
+            sign_out_preference.setOnPreferenceClickListener(
+                    new Preference.OnPreferenceClickListener() {
+                        @Override
+                        public boolean onPreferenceClick(Preference preference) {
+                            Toast.makeText(getContext(), "Sign out", Toast.LENGTH_SHORT).show();
+                            return true;
+                        }
+                    }
+            );
+        }
+
+        if (change_password_button != null) {
+            change_password_button.setOnPreferenceClickListener(
+                    new Preference.OnPreferenceClickListener() {
+                        @Override
+                        public boolean onPreferenceClick(Preference preference) {
+                            Toast.makeText(getContext(), "Change password", Toast.LENGTH_SHORT).show();
+                            return true;
+                        }
+                    }
+            );
+        }
     }
 
 }
+
+
+
+
+
