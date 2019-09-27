790036119bfa76ea4e496cc3f60716840cba27bf
Alexander Powell <ajpowell@email.wm.edu>
2019-09-27 16:30:12 -0700

    remove email verification from preferences

--- app/src/main/java/com/slotmachine/ocr/mic/MySettingsFragment.java
+++ app/src/main/java/com/slotmachine/ocr/mic/MySettingsFragment.java
@@ -48,7 +48,7 @@ public class MySettingsFragment extends PreferenceFragmentCompat {
         Preference change_password_button = findPreference("change_password_button");
         Preference delete_account_button = findPreference("delete_account_button");
         Preference terms_and_conditions_button = findPreference("legal_disclaimer");
-        Preference verify_email_preference_button = findPreference("verify_email_button");
+        //Preference verify_email_preference_button = findPreference("verify_email_button");
         Preference version_number_preference = findPreference("version_number_preference");
 
         //EditTextPreference testPref = findPreference("test_pref");
@@ -236,7 +236,7 @@ public class MySettingsFragment extends PreferenceFragmentCompat {
             );
         }
 
-        if (verify_email_preference_button != null) {
+        /*if (verify_email_preference_button != null) {
 
             verify_email_preference_button.setOnPreferenceClickListener(
                     new Preference.OnPreferenceClickListener() {
@@ -247,7 +247,7 @@ public class MySettingsFragment extends PreferenceFragmentCompat {
                         }
                     }
             );
-        }
+        }*/
     }
 
     /*private void showToast(String message) {
