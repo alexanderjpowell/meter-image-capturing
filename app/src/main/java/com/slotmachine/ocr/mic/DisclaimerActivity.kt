3d085bd7be4b73792d1dc30f77fee7936dddb5c3
Alexander Powell <ajpowell@email.wm.edu>
2021-02-19 21:57:03 -0500

    deleted sign up activity

---
+++ app/src/main/java/com/slotmachine/ocr/mic/DisclaimerActivity.kt
@@ -0,0 +1,11 @@
+package com.slotmachine.ocr.mic
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DisclaimerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_disclaimer)
+    }
+}
